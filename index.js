7e8d3ac5b7661268760848ce4e8bebd983e2b4fb
DrowLink <pauloriveiro01@gmail.com>
2024-04-18 21:09:20 -0400

    setted file

---
+++ index.js
@@ -0,0 +1,53 @@
+const mssql = require("mssql");
+
+// Configuration Data for SQL Server Connection
+const config = {
+  user: 'username',
+  password: 'password',
+  server: 'server',
+  database: 'database_name',
+  port: port,
+  options: {
+    encrypt: false // Usar esto si estás en Windows Azure
+  }
+};
+
+// Function to perform a query to the SQL Server database
+async function querySqlServer(queryString) {
+  try {
+    // Connection to SQL Server
+    await mssql.connect(config);
+    console.log("Successful connection to SQL Server");
+
+    // Make the query
+    const result = await mssql.query(queryString);
+
+    // Print the results
+    console.log("Query results:", result);
+
+    return result.recordset; // return results of query
+  } catch (err) {
+    console.error('Error connecting to SQL Server:', err);
+    return null; // return null in case of error
+  } finally {
+    // Close connection
+    await mssql.close();
+    console.log("Closed sql connection");
+  }
+}
+
+// Ejemplo de uso: realizar una consulta y manejar los resultados
+async function runQuery() {
+  const queryString = `SELECT * FROM vw_account_statment`; // Aquí reemplaza 'tu_tabla' por el nombre de la tabla que deseas consultar
+//const queryString = `SELECT * FROM vw_account_statment WHERE cardcode = '1801'`;
+  const results = await querySqlServer(queryString);
+  if (results) {
+    console.log("Filas retornadas:", results.length);
+    // Puedes procesar los resultados aquí
+  } else {
+    console.log("No se pudieron obtener los resultados de la consulta");
+  }
+}
+
+// Llamar a la función para ejecutar la consulta
+runQuery();
